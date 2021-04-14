d0838d8e0ec2b765e1888faf2c0370a80888574d
faiza10 <faizakurniagck@gmail.com>
2021-04-14 20:54:25 +0700

    commid

---
+++ pertemuan5/BruteForceDivideConquer/src/minggu5/MainNilaiAlgoritma1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu5;
+
+/**
+ *
+ * @author entercomp
+ */
+public class MainNilaiAlgoritma1 {
+    public static void main(String[] args){
+        NilaiAlgoritma1 mhs = new NilaiAlgoritma1 ("Aliyya", 80, 87, 90, 93);
+        System.out.println("Total Nilai Rata-Rata adalah " + mhs.hitungTotalNilai());
+    }
+
+}
