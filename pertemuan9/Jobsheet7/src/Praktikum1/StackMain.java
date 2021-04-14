77a7cc15e4bb457a7ddf4431f536e0e95d064f56
faiza10 <faizakurniagck@gmail.com>
2021-04-14 21:09:38 +0700

    commid

---
+++ pertemuan9/Jobsheet7/src/Praktikum1/StackMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum1;
+
+/**
+ *
+ * @author entercomp
+ */
+import java.util.Scanner;
+public class StackMain {
+    public static void main(String[] args){
+        int pilih ;
+        Scanner input = new Scanner(System.in);
+        Stack stk = new Stack (5);
+        stk.push(15);
+        stk.push(27);
+        stk.push(13);
+    }}
