e7521721b8eccdf668d1990f504357f06f602b96
faiza10 <faizakurniagck@gmail.com>
2021-05-06 12:46:00 +0700

    commited

---
+++ pertemuan11/Jobsheet9/src/Tugas2/SLLMain.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas2;
+
+/**
+ *
+ * @author entercomp
+ */
+public class SLLMain {
+     public static void main(String[] args) {
+        SingleLinkedList singLL = new SingleLinkedList();
+        singLL.addFirst('A');
+        singLL.print();
+        singLL.addLast('C');
+        singLL.print();
+        singLL.insertAt(1, 'B');
+        singLL.print();
+        singLL.insertAfter('C', 'E');
+        singLL.print();
+        singLL.insertBefore('E', 'D');
+        singLL.print();
+
+    }
+}
---
+++ pertemuan11/Jobsheet9/src/Tugas4/Nasabah.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas4;
+
+/**
+ *
+ * @author entercomp
+ */
+public class Nasabah {
+    String nama, alamat, noRek;
+    
+    public Nasabah(String nama, String alamat, String noRek){
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noRek = noRek;
+    }
+}
